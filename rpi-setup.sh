1a60fcd9500998fa67abefe1558b152cc0cfd397
LTCpcgames <LTCpcgames@gmail.com>
2017-10-09 15:37:11 +1100

    Update rpi-setup.sh
    updated version number

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -12,7 +12,7 @@ fi
 # List of packages for all architectures (including Raspbian for x86)
 PACKAGES="pi-greeter sense-hat raspberrypi-ui-mods raspberrypi-artwork raspberrypi-bootloader pix-icons pix-plym-splash rpd-wallpaper rpi-chromium-mods python-sense-emu python3-sense-emu python-sense-emu-doc python3-pip"
 PACKAGES2="lxterminal graphicsmagick fritzing gnome-screenshot mu mono-complete dia-gnome qt4-dev-tools qtcreator python3-pyqt4 qt4-designer vlc python3-pip leafpad lxterminal python-picamera scratch2 python3-thonny ntpdate"
-SCRIPTVERSION="1.5"
+SCRIPTVERSION="1.6"
 SCHOOL=LTC
 # ------------------
 
