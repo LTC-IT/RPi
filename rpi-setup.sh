d4a58e793752cee34caf791235ca2ac2529cfeec
LTC-IT <ryan.cather@ed.act.edu.au>
2022-03-12 15:11:21 +1100

    added sqlite3 package

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -3,7 +3,7 @@
 # ------------------
 # UPDATE THESE VARIABLES AS REQUIRED
 # List of packages for all architectures (including Raspbian for x86)
-PACKAGES_ALL="ntp ntpdate python3-pip vlc gnome-screenshot rpi-chromium-mods python-sense-emu-doc lxterminal graphicsmagick fritzing"
+PACKAGES_ALL="ntp ntpdate python3-pip vlc gnome-screenshot rpi-chromium-mods python-sense-emu-doc lxterminal graphicsmagick fritzing sqlite3"
 PACKAGES_RPI="sense-hat raspberrypi-ui-mods raspberrypi-bootloader python3-sense-emu"
 SCRIPTVERSION="2022.3.12"
 SCHOOL=LTC
