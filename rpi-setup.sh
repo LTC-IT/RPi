c73113e4786b5be76f35ae0efd17709cb0dfe06c
Ryan Cather <49006682+RyanCather@users.noreply.github.com>
2024-03-19 13:39:18 +1100

    fix link

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -95,7 +95,7 @@ echo "Installing pip packages"
 
 # Creating VSCode link
 
-echo "sudo code --user-data-dir /home/pi/.vscode-root --no-sandbox" > ~/Desktop/VSCode.sh 
+echo "sudo code --user-data-dir /home/pi/.vscode-root --no-sandbox" > /home/pi/Desktop/VSCode.sh 
 
 
 # wlan0 & Rpi Camera Configuration
